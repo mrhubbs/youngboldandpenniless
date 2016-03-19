ad4e99a2f9ee96119faaf1ef06c8320ce0c6dfcb
mrhubbs <semberwj@gmail.com>
2016-03-19 13:58:38 -0400

    Revamp and complete site build system, using Plan9 mk.
    Import several posts from Wordpress site to prove out build system.
    
    Rename templates from .html to .jinja to provide needed separation in mkfiles.

---
+++ ybp_builder/render_html.py
@@ -0,0 +1,25 @@
+"""Generic code to render jinja template to html."""
+
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def render_template(template_path, template_fname, context):
+    """Render the given template, using the Jinja engine."""
+    template_environment = Environment(
+        autoescape=False,
+        loader=FileSystemLoader(template_path),
+        trim_blocks=False
+    )
+
+    return template_environment.get_template(
+        template_fname
+    ).render(context)
+
+
+if __name__ == '__main__':
+    import sys
+
+    with open(sys.argv[3], 'w') as out_file:
+        html = render_template(sys.argv[1], sys.argv[2], {})
+        out_file.write(html.encode('utf-8'))
